638e44b70fce6e38687303ed4f0f18acb2a2d63b
Dima_Ostapchuk <Adrenalinbd@gmail.com>
2025-09-04 17:29:36 +0300

    init project, add TypeScript in project

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { persistor, store } from './redux/store.js';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <BrowserRouter>
